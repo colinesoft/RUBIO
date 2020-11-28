7157ce5b654774d41f037629662d6416be38a140
unknown <wilson_mario@terra.com.br>
2020-11-28 11:49:09 -0300

    ATUALIZAÇÃO ARQUIV CONFIG

---
+++ PHP/Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+define ("SVDBNAME","C:\MOVEIS\DADOS\MOVEIS.GDB");
+define ("SVDBHOST", "200.160.107.211");
+define ("SVDBUSER", "SYSDBA");
+define ("SVDBAPASS", "jupiter");
+define ("SVDBCHAR", "utf8");
+define ("SVDBSGDB", "FIREBIRD" );
+
+
+
